45d28391065cb1370cca38426a9d9da28ac4dd8f
Elian Cruz <eliancruz29@gmail.com>
2016-10-04 23:27:23 -0400

    angular-change to add examples of data binding

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Introduction-Angular-2
 
+The tutorial is "Introduction to Angular 2" watched at Microsoft Virtual Academy
+
 When download the project ensure that you have the nodejs version 5.x.x or higher and npm version 3.x.x or higher.
 You can confirm the version by typing node -v, and npm -v
 
--- app/tasks.component.ts
+++ app/tasks.component.ts
@@ -5,17 +5,33 @@ import { Component } from '@angular/core';
   template: `
   <h4 [ngClass]="{red:toggle, blue:!toggle}">This is the Tasks Component</h4>
   <h4 [ngClass]="{red:!toggle, blue:toggle}">This is the second Tasks Component</h4>
+  <span>{{num | json}}</span>
   <h5 *ngIf="!toggle">Hello World!</h5>
   <ul *ngIf="toggle">
     <li *ngFor="let person of people">
       {{ person }}
     </li>
   </ul>
+  <button (click)="onClick()">Click me!</button>
+  <button (mouseenter)="mouseEnter()">Mouse enter!</button>
+  <input [(ngModel)]="sample">
+  <span>{{sample}}</span>
   `,
   styles: [".red {color:red}", ".blue {color:blue}"]
 })
 export class TasksComponent {
-    constructor(){}
+    num: Array<number> = [7,12,15];
     toggle: boolean = true;
     people: Array<string> = ["Person1", "Person2", "Person3"];
+    sample: string = "";
+
+    constructor(){}
+
+    onClick(){
+      alert("Button clicked!!!");
+    }
+
+    mouseEnter(){
+      alert("Mouse entered!!!");
+    }
 }
