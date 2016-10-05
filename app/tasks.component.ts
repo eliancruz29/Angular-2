d9f6ac742e73d78f5de9ff994dddcc98f841123a
Elian Cruz <eliancruz29@gmail.com>
2016-10-04 20:52:14 -0400

    angular-change to add the examples of using the structural directives

--- app/tasks.component.ts
+++ app/tasks.component.ts
@@ -5,10 +5,17 @@ import { Component } from '@angular/core';
   template: `
   <h4 [ngClass]="{red:toggle, blue:!toggle}">This is the Tasks Component</h4>
   <h4 [ngClass]="{red:!toggle, blue:toggle}">This is the second Tasks Component</h4>
+  <h5 *ngIf="!toggle">Hello World!</h5>
+  <ul *ngIf="toggle">
+    <li *ngFor="let person of people">
+      {{ person }}
+    </li>
+  </ul>
   `,
   styles: [".red {color:red}", ".blue {color:blue}"]
 })
 export class TasksComponent {
     constructor(){}
-    toggle: boolean = false;
+    toggle: boolean = true;
+    people: Array<string> = ["Person1", "Person2", "Person3"];
 }
